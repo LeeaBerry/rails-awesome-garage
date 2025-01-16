010db9180e72d308b1053ef9f89f3623b302df4d
Leea Berry <leeaberry@gmail.com>
2025-01-16 14:42:02 +0000

    added carcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "cars#index"
+  resources :cars, only: [:index, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,50 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning the database!"
+Car.destroy_all
+Owner.destroy_all
+
+
+puts "Creating new Owners"
+
+owner = Owner.create!(
+  nickname: "zoomzoom"
+)
+
+owner1 = Owner.create!(
+  nickname: "oldgal"
+)
+
+owner2 = Owner.create!(
+  nickname: "bluebaby"
+)
+
+puts "Creating new Cars"
+
+Car.create!(
+  brand: "Volkswagen",
+  model: "Jetta",
+  year: "2023",
+  fuel: "Regular",
+  owner: owner
+)
+
+Car.create!(
+  brand: "Honda",
+  model: "Pilot FWD",
+  year: "2025",
+  fuel: "Premimum",
+  owner: owner1
+)
+
+Car.create!(
+  brand: "Volvo",
+  model: "XC40",
+  year: "2025",
+  fuel: "Premimum",
+  owner: owner2
+)
+
+puts "All done! created"
